b8c8d95e238f7f1a9b55cf3fe10d822518ee365f
Carlos <carlosfb2005@gmail.com>
2025-03-23 21:42:24 +0100

    rutas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { ReservasComponent } from './views/reservas/reservas.component';
+import { ContactameComponent } from './views/contactame/contactame.component';
+import { MiPerfilComponent } from './views/mi-perfil/mi-perfil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'inicio', component: InicioComponent},
+    {path: 'contactame', component: ContactameComponent},
+    {path: 'reservas', component: ReservasComponent},
+    {path: 'perfil', component: MiPerfilComponent},
+
+
+    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+
+];  
