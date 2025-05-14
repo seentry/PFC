8be298146d5117fb4b673cdaf07cbd4d8c35f540
cfernandezb <cfernandez@hiberus.com>
2025-05-14 08:56:57 +0200

    intentar ver trajes reservados

--- front/src/app/views/mi-perfil/mi-perfil.component.ts
+++ front/src/app/views/mi-perfil/mi-perfil.component.ts
@@ -1,71 +1,64 @@
-import { Component } from '@angular/core';
-import {RequestService} from '../../services/request.service';
-import {Router} from '@angular/router';
-import {Traje, Padre} from '../../models/response.interface';
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../../services/request.service';
+import { Router } from '@angular/router';
+import { Traje, Padre } from '../../models/response.interface';
 
 @Component({
   selector: 'app-mi-perfil',
   standalone: true,
   imports: [],
   templateUrl: './mi-perfil.component.html',
-  styleUrl: './mi-perfil.component.css'
+  styleUrls: ['./mi-perfil.component.css']
 })
-export class MiPerfilComponent {
+export class MiPerfilComponent implements OnInit {
   public parent: Padre | null = null;
   public reservedTrajes: Traje[] = [];
 
   public urlPadres = 'http://localhost:8001/api/padres/';
-  public urlTrajes = 'http://localhost:8001/api/trajes';
-  public userId = Number(localStorage.getItem('userId'));
+  public urlTrajes  = 'http://localhost:8001/api/trajes/';
+  public mainId     = Number(localStorage.getItem('userId'));
 
-  constructor(public service: RequestService, protected router: Router) {
-  }
+  constructor( public service: RequestService, public router: Router ) {}
 
-   public ngOnInit(): void {
-    if (!this.userId) {
+  public ngOnInit(): void {
+    if (!this.mainId) {
       this.router.navigate(['login']);
       return;
     }
-    this.loadParent();
-    this.loadReservedTrajes();
-  }
 
-  public loadParent(): void {
-    this.service.getPadres(this.urlPadres + this.userId).subscribe({
-      next: padre => {
-        this.parent = padre as any;
-      },
-      error: err => console.error('No se pudo cargar el padre:', err)
-    });
-  }
+    this.service.getPadre(this.urlPadres + this.mainId).subscribe({
+    next: (padre: Padre) => {
+      this.parent = padre;
+    },
+    error: err => console.error('No se pudo cargar el padre:', err)
+  });
 
-  public loadReservedTrajes(): void {
     this.service.getTrajes(this.urlTrajes).subscribe({
-      next: all => {
-        this.reservedTrajes = all.filter(t =>
-          t.reservadoPor === this.userId
-        );
-        console.log('Trajes reservados por mí:', this.reservedTrajes);
+      next: (all: Traje[]) => {
+        this.reservedTrajes = all
+          .filter(t => t.reservadoPor === this.mainId)
+          .map(t => {
+            t.fechaIncorporacion = new Date(t.fechaIncorporacion)
+              .toLocaleDateString('es-ES');
+            return t;
+          });
+        console.log('Trajes reservados:', this.reservedTrajes);
       },
       error: err => console.error('Error cargando trajes:', err)
     });
   }
 
   public cancelTraje(id: number): void {
-    this.service.deleteTraje(`${this.urlTrajes}/${id}`).subscribe({
-      next: () => this.loadReservedTrajes(),
+    this.service.deleteTraje(this.urlTrajes + id).subscribe({
+      next: () => {
+        this.reservedTrajes = this.reservedTrajes.filter(t => t.id !== id);
+      },
       error: err => console.error('Error eliminando traje:', err)
     });
   }
 
-  get user(): Padre | null {
-    return this.parent;
-  }
-
-  public logout() {
-    localStorage.clear()
-    this.router.navigate(['/inicio']).then(() => {
-      window.location.reload();
-    });  
+  public logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/inicio']).then(() => location.reload());
   }
 }
